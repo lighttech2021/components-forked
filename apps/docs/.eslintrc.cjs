00fa19430d9b3e0606e601ea488734f55f41aed1
ahmadshaheer <ashaheerki@gmail.com>
2024-10-03 23:26:13 +0430

    chore: module.exports to export default

--- apps/docs/.eslintrc.cjs
+++ apps/docs/.eslintrc.cjs
@@ -1,4 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
-module.exports = {
+export default {
   extends: ["@repo/eslint-config/storybook.js"],
 };
--- packages/input/tailwind.config.js
+++ packages/input/tailwind.config.js
@@ -1,7 +1,7 @@
-const sharedConfig = require("@signozhq/tailwind-config/tailwind.config.js");
+import sharedConfig from "@signozhq/tailwind-config/tailwind.config.js";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   ...sharedConfig,
   content: [...sharedConfig.content, "./src/**/*.{js,ts,jsx,tsx}"],
 };
