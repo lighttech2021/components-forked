6c6d3533dc94783033bfd5f0824f5f21ec427182
ahmadshaheer <ashaheerki@gmail.com>
2024-10-08 21:42:26 +0430

    feat: setup typescript check, prettier, eslint, conventional commits, prevent push to main branch

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+export default {
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 1,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	semi: true,
+};
--- apps/packages-playground/src/main.tsx
+++ apps/packages-playground/src/main.tsx
@@ -1,23 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ThemeProvider, useTheme } from "@signozhq/theme";
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { ThemeProvider, useTheme } from '@signozhq/theme';
 
 function ThemedApp() {
-  const { theme } = useTheme();
+	const { theme } = useTheme();
 
-  return (
-    <div className={`app ${theme}`}>
-      <App />
-    </div>
-  );
+	return (
+		<div className={`app ${theme}`}>
+			<App />
+		</div>
+	);
 }
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider>
-      <ThemedApp />
-    </ThemeProvider>
-  </StrictMode>
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<ThemeProvider>
+			<ThemedApp />
+		</ThemeProvider>
+	</StrictMode>,
 );
