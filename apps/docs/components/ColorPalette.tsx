81edfc4398758fd2cfe43a46f514c2f6f05b72b3
ahmadshaheer <ashaheerki@gmail.com>
2024-10-23 14:20:21 +0430

    feat: add gradients to color story

--- apps/docs/components/ColorPalette.tsx
+++ apps/docs/components/ColorPalette.tsx
@@ -7,27 +7,54 @@ function ColorPalette() {
     <div className="p-5">
       <h1 className="mb-5 text-lg font-bold text-vanilla-100">Pallette</h1>
 
+      {/* Regular colors */}
       <div className="grid grid-cols-4 gap-5 mb-12">
-        {colors.map((color) => (
+        {colors
+          .filter((item) => item.name !== "Gradient")
+          .map((color) => (
+            <div key={color.name}>
+              <h3 className="mt-4 text-base font-bold capitalize text-vanilla-100">
+                {color.name}
+              </h3>
+              <div className="overflow-hidden rounded-lg shadow-lg">
+                {color.shades.map((shade) => (
+                  <div
+                    key={shade.name}
+                    className={`flex items-center justify-between w-full h-12 px-4 font-semibold ${color.name === "ink" || color.name === "slate" ? "text-vanilla-100" : ""}`}
+                    style={{ backgroundColor: shade.value }}
+                  >
+                    <span>{shade.name}</span>
+                    <span className="uppercase">{shade.value}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
+
+      {/* Gradients */}
+      {colors
+        .filter((item) => item.name === "Gradient")
+        .map((color) => (
           <div key={color.name}>
-            <h3 className="mt-4 text-base font-bold capitalize text-vanilla-100">
-              {color.name}
-            </h3>
-            <div className="overflow-hidden rounded-lg shadow-lg">
+            <h1 className="mb-5 text-lg font-bold text-vanilla-100">
+              Gradients
+            </h1>
+
+            <div className="grid grid-cols-6 gap-5 mb-12">
               {color.shades.map((shade) => (
-                <div
-                  key={shade.name}
-                  className={`flex items-center justify-between w-full h-12 px-4 font-semibold ${color.name === "ink" || color.name === "slate" ? "text-vanilla-100" : ""}`}
-                  style={{ backgroundColor: shade.value }}
-                >
-                  <span>{shade.name}</span>
-                  <span className="uppercase">{shade.value}</span>
+                <div className=" shadow-lg">
+                  <div
+                    key={shade.name}
+                    className={`flex items-center justify-between w-full h-20 px-4 font-semibold overflow-hidden rounded-lg`}
+                    style={{ backgroundImage: shade.value }}
+                  ></div>
+                  <span className="text-vanilla-100">{shade.name}</span>
                 </div>
               ))}
             </div>
           </div>
         ))}
-      </div>
     </div>
   );
 }
